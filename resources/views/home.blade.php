9011c2911bf11e37da14c3553efd97f62a72be34
L200190217 <suryasumirat678@gmail.com>
2022-03-01 15:51:58 +0700

    upload file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,202 @@
+@extends('layouts.main')
+
+@section('container')
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="style.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+
+    <!-- <div class="bacground"> -->
+    <section class="portfolio2">
+        <div class="container">
+            <div class="row text-center pt-4">
+                <div class="products-judul col-lg-12">
+                    <h3>Our Products</h3>
+                </div>
+                <div class="products-text">
+                    <p>With full of hope we make products with all our heart and dedication, to help the wider community
+                        solve their problems.</p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="products-card col-lg-4 col-md-6">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4;">
+                            <div class="" style="height: 280px;">
+
+                            </div>
+                            <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                <div class="products-judul2">
+                                    <h5>labormate</h5>
+                                </div>
+                                <center>
+                                    <p class="products-text2">Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                                    <div class="tombol-learn-more" style="width: 12rem;">
+                                        <h5>Learn More</h5>
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="products-card col-lg-4 col-md-6">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4;">
+                            <div style="height: 280px;">
+                            </div>
+                            <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                <div class="products-judul2">
+                                    <h5>catmate</h5>
+                                </div>
+                                <center>
+                                    <p class="products-text2">Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit,
+                                        sed do eiusmod tempor incididunt ut labore.</p>
+                                    <div class="tombol-learn-more" style="width: 12rem;">
+                                        <h5>Learn More</h5>
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="products-card col-lg-4 col-md-6">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4;">
+                            <div style="height: 280px;">
+                            </div>
+                            <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                <div class="products-judul2">
+                                    <h5>marrymate</h5>
+                                </div>
+                                <center>
+                                    <p class="products-text2">Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing
+                                        elit,
+                                        sed do eiusmod tempor incididunt ut labore.</p>
+                                    <div class="tombol-learn-more" style="width: 12rem;">
+                                        <h5>Learn More</h5>
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <center>
+                <div class="tombol-explore">
+                    <h5>Explore</h5>
+                </div>
+            </center>
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+    <section class="portfolio2">
+        <div class="container">
+            <div class="row text-center pt-4">
+                <div class="products-judul col-lg-12">
+                    <h3>What’s on Tupai</h3>
+                </div>
+                <div class="products-text">
+                    <p>Our stories and latest updates. Find everything you want to know about u, and reach development towards success
+                        together.</p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="products-card col-lg-4 col-md-6">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4;">
+                            <div class="" style="height: 280px;">
+
+                            </div>
+                            <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                <div class="news-judul2">
+                                    <h5>Quality you can trust</h5>
+                                </div>
+                                <p class="news-tanggal">19 Februari 2022</p>
+                                <p class="news-text2">Startup Framework works fine on devices supporting Retina Desplay. Feel the clarity!</p>
+                                <p class="tombol-read-more">Read More <img src="image/panah-right.svg" alt=""> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="products-card col-lg-4 col-md-6">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4;">
+                            <div style="height: 280px;">
+                            </div>
+                            <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                <div class="news-judul2">
+                                    <h5>Quality you can trust</h5>
+                                </div>
+                                <p class="news-tanggal">19 Februari 2022</p>
+                                <p class="news-text2">Startup Framework works fine on devices supporting Retina Desplay. Feel the clarity!</p>
+                                <p class="tombol-read-more">Read More <img src="image/panah-right.svg" alt=""> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="products-card col-lg-4 col-md-6">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4;">
+                            <div style="height: 280px;">
+                            </div>
+                            <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                <div class="news-judul2">
+                                    <h5>Quality you can trust</h5>
+                                </div>
+                                <p class="news-tanggal">19 Februari 2022</p>
+                                <p class="news-text2">Startup Framework works fine on devices supporting Retina Desplay. Feel the clarity!</p>
+                                <p class="tombol-read-more">Read More <img src="image/panah-right.svg" alt=""> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <center>
+                <div class="tombol-explore">
+                    <h5>Explore</h5>
+                </div>
+            </center>
+
+        </div>
+    </section>
+
+</body>
+
+</html>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="">
+        @yield('container')
+    </div>
+
+    
+</body>
+
+</html>
