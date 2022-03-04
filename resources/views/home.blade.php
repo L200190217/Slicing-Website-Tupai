772d5676b212ff53e36cbad0dcc6fde547679fcf
L200190217 <suryasumirat678@gmail.com>
2022-03-04 15:59:40 +0700

    membuat halaman tagline section, about US, & footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -179,21 +179,77 @@
 
 
 
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="about">
+            <div class="row">
+                <div class="about-card col-lg-6 col-md-6 ">
+                    <div class="flex-row" style="margin: auto; ">
+                        <h3>Jump with tecnology, Land with the goal</h3>
+                        <p>We are a team that is full of enthusiasts and always ready to find the right path and
+                            direction to deal with the current
+                            development challenges. Tupai Tech provides various services needed and always provides the
+                            best services to every party
+                            to reach the expected future together with integrity and professionalism.</p>
+                        <div class="tombol">
+                            <h6>Explore</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="about-card col-lg-6 col-md-6 ">
+                    <img src="image/Galeri About.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="background-tagline">
+    <img src="image/background tagline.svg" alt="">
+</div>
 
 
+<div class="background-about">
+    <img src="image/background about.svg" alt="">
+</div>
 
+<div class="container-fluid py-5">
+    <div class="container">
+
+        <div class="">
+            <div class="about">
+                <div class="row">
+                    <div class="about-card col-lg-6 col-md-6 ">
+                        <img src="image/Galeri About.svg" alt="">
+                    </div>
+                    <div class="about-card col-lg-6 col-md-6 ">
+                        <div class="" style="margin: auto; ">
+                            <h3>About Us</h3>
+                            <p>Tupai Tech is a IT Consutant that provides various services to answer the challenges of a
+                                fast-moving era. Tupai Tech
+                                always provides the best service for every party from all regions in Indonesia with
+                                integrity and professionalism.</p>
+                            <div class="tombol">
+                                <h6>Explore</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- <div class="bacground"> -->
 <section class="portfolio2">
     <div class="background-why">
-        <img src="image/background why.svg" alt="" style="width: 100%;">
+        <img src="image/background why.svg" alt="">
     </div>
     <div class="container">
         <div class="row text-center pt-4">
-
         </div>
         <div class="row justify-content-center">
-
-
-
             <div class="products-card col-lg-4 col-md-12">
                 <h3 id="why-h3">Why Should You Choose Tupai Tech?</h3>
                 <div class="why-text">
@@ -204,14 +260,9 @@
                 </div>
                 <div class="mx-auto" style="width: 100%;">
                     <div class="card" style="width: 100%; border-radius: 10px; background-color: #C4C4C4; height: 429px;margin-top: -20px;">
-
                     </div>
                 </div>
-
             </div>
-
-
-
             <div class="products-card2 col-lg-3 col-md-6">
                 <div class="mx-auto" style="height: 50%;">
                     <div class="card" style="height: 96%; border-radius: 10px; padding: 24px;">
@@ -237,10 +288,6 @@
                 </div>
             </div>
 
-
-
-
-
             <div class="products-card2 col-lg-3 col-md-6">
                 <div class="mx-auto" style="height: 50%; ">
                     <div class="card" style="height: 96%; border-radius: 10px; padding: 24px;">
@@ -265,12 +312,7 @@
                 </div>
 
             </div>
-
-
-
-
         </div>
-
     </div>
 </section>
 
@@ -359,8 +401,6 @@
 </section>
 
 
-
-
 <section class="portfolio2">
     <div class="container">
         <div class="row text-center pt-4">
@@ -371,17 +411,8 @@
                 <p>Prove it and get significant development for yourself and your business, as our colleagues say</p>
             </div>
         </div>
-
-
-
-
-
-
-
         <div class="row justify-content-center">
-
             <div class="owl-carousel owl-theme">
-
                 <div class="products-card">
                     <div class="mx-auto" style="width: 100%;">
                         <div style="margin-top: 20px;">
@@ -430,14 +461,31 @@
                     </div>
                 </div>
 
+                <div class="products-card">
+                    <div class="mx-auto" style="width: 100%;">
+                        <div style="margin-top: 20px; ">
+                            <img class="" src="image/capture2.svg" style="height: 70px;">
+                        </div>
+                        <div class="" style="margin-left: 72px;">
+                            <div class="" style="width: 100%; margin-top: -90px;">
+
+                                <div class="card-body" style="background-color: white; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px;">
+                                    <p class="products-text3">Worth every penny, if you have basic HTML knowledge. It helped us get an
+                                        exceptional landing page up and running in no
+                                        time
+                                    </p>
+                                    <h5 class="testimoni-judul2">
+                                        Austin Campbell
+                                    </h5>
+                                    <p class="testimoni-transparan">Dropbox</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
-
     </div>
-
-
-
 </section>
 
 
@@ -446,6 +494,7 @@
         loop: true,
         margin: 10,
         // nav: true,
+        dots: true,
         responsive: {
             0: {
                 items: 1
@@ -459,6 +508,49 @@
         }
     })
 </script>
+
+
+
+<div class="footer-utama">
+    <div class="container-fluid py-5">
+        <div class="container">
+
+
+            <div class="row" style="color: white;">
+                <div class="footer col-lg-2 col-md-5 col-sm-5 me-3 mb-4">
+                    <img src="image/Logo Tupai.svg" alt="">
+                </div>
+                <div class="footer col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <h6>Address</h6>
+                    <p>San Francisco, CA
+                        560 Bush St & 20th Ave, Apt
+                        +1 555 333 44 22</p>
+                </div>
+                <div class="footer col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <h6>Try Startup for Free</h6>
+                    <p>Start a 14 Day Free Trial on any of our paid plans. No credit card required.</p>
+                </div>
+                <div class="footer col-lg-2 col-md-4 col-sm-6 mb-4">
+                    <h6>Stay Tuned</h6>
+                    <img src="image/fa-twitter.svg" alt="">
+                    <img src="image/fa-facebook-square.svg" alt="">
+                    <img src="image/fa-google-plus.svg" alt="">
+                </div>
+
+                <div class="footer col-lg-1 col-md-4 col-sm-6 mb-4">
+                    <button>Get Started</button>
+                </div>
+
+                <hr />
+                <div class="footer">
+                    <p class="text-center">Be sure to take a look at our Terms of Use and Privacy Policy</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 
 
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,17 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400&display=swap" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="style.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <link rel="stylesheet" href="owl.carousel.min.css">
+
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
